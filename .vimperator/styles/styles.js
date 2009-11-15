91d558f5b95057117057d9dedbf437b464872fba
Lucas de Vries <lucasdevries@gmail.com>
2009-11-15 01:06:19 +0100

    vimperator-styles: update userstyle structure, add tvtorrents userstyle

--- .vimperator/styles/styles.js
+++ .vimperator/styles/styles.js
@@ -1,47 +1,64 @@
-//
-// Adds all the userstyles to vimperator
-//
-//
-
-/// Get style directory
-var styledir = io.getRuntimeDirectories("styles")[0].path;
+/**
+ * Adds all the userstyles to vimperator
+ *
+ */
 
 /// Add styles
-// Wikipedia style
-styles.addSheet(false,
-    // Name
-    "Wikipedia:Minimal",
-    // Filter
-    "en.wikipedia.org",
-    // File
-    io.readFile(styledir+"/wikipedia.css")
-)
+liberator.userstyles = [
+    [
+        "wikipedia:minimal",
+        "en.wikipedia.org",
+        "wikipedia.css",
+    ],
 
-// Scroogle style
-styles.addSheet(false,
-    // Name
-    "Scroogle:Minimal",
-    // Filter
-    "https://ssl.scroogle.org/cgi-bin/nbbwssl.cgi",
-    // File
-    io.readFile(styledir+"/scroogle.css")
-)
+    [
+        "scroogle:minimal",
+        "https://ssl.scroogle.org/cgi-bin/nbbwssl.cgi",
+        "scroogle.css",
+    ],
 
-// Reddit style
-styles.addSheet(false,
-    // Name
-    "Reddit:Minimal",
-    // Filter
-    "reddit.com",
-    // File
-    io.readFile(styledir+"/reddit.css")
-)
+    [
+        "reddit:minimal",
+        "reddit.com",
+        "reddit.css",
+    ],
 
-styles.addSheet(false,
-    // Name
-    "ArchlinuxForums:Minimal",
-    // Filter
-    "bbs.archlinux.org",
-    // File
-    io.readFile(styledir+"/archlinux.forum.css")
-)
+    [
+        "archlinux-forums:minimal",
+        "bbs.archlinux.org",
+        "archlinux.forum.css",
+    ],
+
+    [
+        "tvtorrents:minimal",
+        "tvtorrents.com",
+        "tvtorrents.css",
+    ]
+];
+
+/// Command to load all styles
+var loaduserstyles = function()
+{
+    // Get style directory
+    var styledir = io.getRuntimeDirectories("styles")[0].path;
+
+    for ([i, [name, filter, file]] in Iterator(liberator.userstyles))
+    {
+      // Remove all sheets with this filter
+      styles.removeSheet(false, name)
+
+      // Add the sheet
+      styles.addSheet(false, name, filter, 
+      io.readFile(styledir+"/"+file))
+    }
+}
+
+// Initial load
+loaduserstyles()
+
+// Add it as a command
+commands.add(
+    ["loaduserstyles"],
+    "Load all user styles",
+    loaduserstyles
+);
