acae47804a2e63d2971e3a8c31f1414ef4f7209a
Lucas de Vries <lucas@glacicle.org>
2010-03-29 05:05:59 +0000

    ranger: use new options

--- .ranger/options.py
+++ .ranger/options.py
@@ -2,9 +2,10 @@ from ranger.api.options import *
 
 colorscheme = "oblivion"
 show_cursor = False
+draw_bookmark_borders = False
 
 sort = "basename"
+sort_case_insensitive = True
 
 autosave_bookmarks = False
 flushinput = False
-case_insensitive = True
